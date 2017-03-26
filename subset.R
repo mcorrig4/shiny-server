d21131a3b7d862fd4f2e41d40ccf49e6e6147e3d
mgencare <mgencare@gmail.com>
2017-03-26 19:43:11 -0400

    Update subset.R

--- subset.R
+++ subset.R
@@ -1,118 +1,71 @@
 # Stats Project
-# -----------------------
+# ======================
 
-# Load dataset
-#---------------------------------------
-loadData <- function(filepath, typ="H") {
-	df <- read.csv(filepath)
-	if (typ == "L") {
-		df <- df[1:1000,]
-	}
-	return(df)
-}
-# Descriptive Statistics
-#---------------------------------------
-susbsetData <- function(data, loc, tim, typ) {
-	# Inputs:
-	#	loc - a list containing 2 elements: vector(latitude/longitude) for bottom-left and top-right points
-	#	tim - a list containing 2 elements: vector(start and end time), interval (Y, M, D)
-	#	typ - a list containing 1 element:  vector(primary type)
-	#--------------------------------------------------------------------------
-	df <- data
-	df <- df[  !is.na(df$Latitude)
-			 & !is.na(df$Longitude)
-			 & !is.na(df$Date)
-			 & !is.na(df$Primary.Type),]
-	
-	# Subset by type
-	# ------------------
-	df <- df[df$Primary.Type %in% typ$prm,]
-	# Subset by location
-	# ------------------
-	df <- df[df$Latitude >= loc$bl[1] 
-			 & df$Latitude <= loc$tr[1]
-			 & df$Longitude >= loc$bl[2]	
-			 & df$Longitude <= loc$tr[2],]
-	# Subset by time
-	# ------------------
-	df$Date <- as.POSIXct(as.character(df$Date),format="%m/%d/%Y %I:%M:%S %p", tz="CST6CDT")
-	
-	df$Year <- year(df$Date)
-	df$Month <- month(df$Date)
-	df$Day <- day(df$Date)
-	df$Hour <- hour(df$Date)
-	df <- switch(tim$int,
-				 "Y" = df[df$Year>=tim$pd[1] & df$Year<=tim$pd[2],],
-				 "M" = df[df$Month>=tim$pd[1] & df$Month<=tim$pd[2],],
-				 "D" = df[df$Day>=tim$pd[1] & df$Day<=tim$pd[2],])
-	
-	return(df)
-}
-
-# Descriptive Graphs
-# ------------------
-plotGraph <- function(df) {
-	df.grp <- df %>%
-		group_by(Hour)
-	
-	ggplot(df.grp, aes(Hour, fill=Primary.Type)) +
-		geom_histogram(binwidth = 1)
-}
-
-
-
-
-# MAIN PROGRAM
-# =======================
+# Include Packages
+# ======================
+library(readr)
 library(lubridate)
 library(dplyr)
 library(ggplot2)
-
-
 setwd("C://Users//mgencare//Documents//2. Academics//2. Academic 2017-2018//3. Courses//1. BUSS - Business Statistics//Project")
 
-crime.data <- loadData("Crimes-2001-present.csv")
+# Load raw data
+# ======================
+df.raw <- read_csv("Crimes-2001-present.csv")
+
+# Manually adjust column types
+#-----------------------
+df.raw$`Primary Type` <- as.factor(df.raw$`Primary Type`)
+df.raw$`Description` <- as.factor(df.raw$`Description`)
+df.raw$`Location Description` <- as.factor(df.raw$`Location Description`)
+df.raw$Date <- as.POSIXct(df.raw$Date, format="%m/%d/%Y %I:%M:%S %p", tz="CST6CDT")
+df.raw$Year <- year(df.raw$Date)
+df.raw$Month <- month(df.raw$Date)
+df.raw$Day <- day(df.raw$Date)
+df.raw$Hour <- hour(df.raw$Date)
+
+
+# Filter data based on: location, time, and crime type
+# ======================
+# Inputs:
+#	loc - a list containing 2 elements: vector(latitude/longitude) for bottom-left and top-right points
+#	tim - a list containing 2 elements: vector(start and end time), interval (Y, M, D)
+#	typ - a list containing 1 element:  vector(primary type)
 tim <- list(pd=c(2003,2016), int="Y")
 typ <- list(prm=c("ASSAULT", "BATTERY"))
 loc <- list(bl=c(min(df.raw$Latitude, na.rm=T), min(df.raw$Longitude, na.rm=T)), 
 			tr=c(max(df.raw$Latitude, na.rm=T), max(df.raw$Longitude, na.rm=T)))
 
 
-graph.data <- subsetData(crime.data, loc, tim, typ)
-plotGraph(graph.data)
+df <- df.raw %>%
+	filter(!is.na(Latitude), !is.na(Longitude), !is.na(Date), !is.na(`Primary Type`)) %>%
+	filter(`Primary Type` %in% typ$prm) %>%
+	filter(Latitude >= loc$bl[1], Latitude <= loc$tr[1], Longitude >= loc$bl[2], Longitude <= loc$tr[2])%>%
+	filter(switch(tim$int,
+		   "Y" = (Year>=tim$pd[1] & Year<=tim$pd[2]),
+		   "M" = (Month>=tim$pd[1] & Month<=tim$pd[2]),
+		   "D" = (Day>=tim$pd[1] & Day<=tim$pd[2]) ))
 
 
 
+# Descriptive Statistics and Graphs
+# ======================
+# (using HOMICIDE)
+df.hom <- df.raw %>% filter(`Primary Type`=="HOMICIDE")
 
 
+# 1. Calculate the mean time of murders
+df.hr <- df.hom %>%
+	group_by(`Primary Type`) %>%
+	summarise(mean_hour=median(Hour,na.rm=T))
+df.hr
+
+ggplot(df.hom, aes(x=Hour, fill=`Primary Type`)) +
+	geom_histogram(binwidth = 1, position="stack")
 
 
-# Scrap
-#---------------------------------------
-# levels(df.raw$Primary.Type)
-# df.typ <- group_by(df.raw, Primary.Type)
-# summarise(df.typ, count=length(Primary.Type))
-# 
-# levels(df.raw$Description)
-# df.dsc <- group_by(df.raw, Description)
-# summarise(df.dsc, count=length(Description))
-
-
-# ggplot(df.sex_assault, aes(Hour, fill=Primary.Type)) +
-# 	geom_histogram(binwidth = 1)
-
-
-# df2 <- df %>%
-# 	group_by(Primary.Type) %>%
-# 	#filter(Primary.Type %in% c("CRIM SEXUAL ASSAULT", "SEX OFFENSE")) %>%
-# 	droplevels()
-# 
-# # Display graph
-# df.sex_assault <- df.typ %>%
-# 	filter(Primary.Type %in% c("CRIM SEXUAL ASSAULT", "SEX OFFENSE")) %>%
-# 	droplevels()
-# 
-# df.sex_assault$Year <- year(df.sex_assault$Date)
-# df.sex_assault$Month <- month(df.sex_assault$Date)
-# df.sex_assault$Day <- day(df.sex_assault$Date)
-# df.sex_assault$Hour <- hour(df.sex_assault$Date)
+# 2. Calcualte the total amount of the crime type for each year
+df.yr <- df.hom %>%
+	group_by(`Primary Type`, Year) %>%
+	summarise(total_year=n())
+as.data.frame(df.yr)
